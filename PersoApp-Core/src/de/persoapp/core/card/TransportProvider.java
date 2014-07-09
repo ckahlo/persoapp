d0ad79e5b5ecbb62cbd80482eb3d715f7b634b2f
Christian Kahlo <Christian Kahlo@1296ccec-e324-93b5-48e2-b0d15f74e564>
2014-07-09 14:20:06 +0000

    corrected / completed comments

--- PersoApp-Core/src/de/persoapp/core/card/TransportProvider.java
+++ PersoApp-Core/src/de/persoapp/core/card/TransportProvider.java
@@ -1,96 +1,94 @@
 /**
- *
+ * 
  * COPYRIGHT (C) 2010, 2011, 2012, 2013, 2014 AGETO Innovation GmbH
- *
+ * 
  * Authors Christian Kahlo, Ralf Wondratschek
- *
+ * 
  * All Rights Reserved.
- *
+ * 
  * Contact: PersoApp, http://www.persoapp.de
- *
+ * 
  * @version 1.0, 30.07.2013 13:50:47
- *
+ * 
  *          This file is part of PersoApp.
- *
+ * 
  *          PersoApp is free software: you can redistribute it and/or modify it
  *          under the terms of the GNU Lesser General Public License as
  *          published by the Free Software Foundation, either version 3 of the
  *          License, or (at your option) any later version.
- *
+ * 
  *          PersoApp is distributed in the hope that it will be useful, but
  *          WITHOUT ANY WARRANTY; without even the implied warranty of
  *          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  *          Lesser General Public License for more details.
- *
+ * 
  *          You should have received a copy of the GNU Lesser General Public
  *          License along with PersoApp. If not, see
  *          <http://www.gnu.org/licenses/>.
- *
+ * 
  *          Diese Datei ist Teil von PersoApp.
- *
+ * 
  *          PersoApp ist Freie Software: Sie können es unter den Bedingungen der
  *          GNU Lesser General Public License, wie von der Free Software
  *          Foundation, Version 3 der Lizenz oder (nach Ihrer Option) jeder
  *          späteren veröffentlichten Version, weiterverbreiten und/oder
  *          modifizieren.
- *
+ * 
  *          PersoApp wird in der Hoffnung, dass es nützlich sein wird, aber OHNE
  *          JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
  *          Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN
  *          ZWECK. Siehe die GNU Lesser General Public License für weitere
  *          Details.
- *
+ * 
  *          Sie sollten eine Kopie der GNU Lesser General Public License
  *          zusammen mit diesem Programm erhalten haben. Wenn nicht, siehe
  *          <http://www.gnu.org/licenses/>.
- *
+ * 
  */
 package de.persoapp.core.card;
 
 /**
  * <p>
- * The <tt>TransportProvider</tt> is an abstract, generic interface with the
- * intent to unify the communication to the higher application layers. The
- * properties of the installed hardware can be retrieved through the provided
- * methods.
+ * The <tt>TransportProvider</tt> interface defines basic functionality to be
+ * supported by every transport provider.
  * </p>
- * <p>
- * <code>public interface TransportProvider</code>
- * </p>
+ * 
+ * @see JSCIOTransport
+ * @see ISOSMTransport
+ * @see PersoSimTransport
  * 
  * @author Christian Kahlo
  * @author Rico Klimsa - added javadoc comments.
  */
 public interface TransportProvider {
-	
+
 	/**
-	 * Returns the parent object of the underlying object.
+	 * Returns the parent of this TransportProvider if applicable.
 	 * 
-	 * @return Returns the parent object of the underlying object.
+	 * @return parent or null if no parent exists
 	 */
 	public Object getParent();
 
 	/**
-	 * Transmits the provided <em>APDU</em> and returns the <em>APDU</em>
-	 * -response. The success of the operation can be seen at the transmitted
-	 * status word, at the response.
+	 * Transmit <em>APDU</em> through this TransportProvider and return
+	 * response.
 	 * 
 	 * @param apdu
-	 *            - The apdu, to transmit.
+	 *            - APDU to be transmitted
 	 * 
-	 * @return Returns the response.
+	 * @return response from card
 	 */
 	public byte[] transmit(byte[] apdu);
 
 	/**
-	 * Returns the last status word.
+	 * Returns the last received status word.
 	 * 
-	 * @return Returns the last status word.
+	 * @return status word of last transmitted APDU
 	 */
 	public int lastSW();
 
 	/**
-	 * Terminates the activities of the calling transport provider.
+	 * Closes this transport provider
 	 */
 	public void close();
 }

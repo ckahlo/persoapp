4c8c1975d02752a48bf91b00c95ad12c28718fbf
Christian Kahlo <Christian Kahlo@1296ccec-e324-93b5-48e2-b0d15f74e564>
2014-07-11 10:02:11 +0000

    corrected comments

--- trunk/PersoApp-Core/src/de/persoapp/core/ws/IFDService.java
+++ trunk/PersoApp-Core/src/de/persoapp/core/ws/IFDService.java
@@ -1,49 +1,49 @@
 /**
- *
+ * 
  * COPYRIGHT (C) 2010, 2011, 2012, 2013, 2014 AGETO Innovation GmbH
- *
+ * 
  * Authors Christian Kahlo, Ralf Wondratschek
- *
+ * 
  * All Rights Reserved.
- *
+ * 
  * Contact: PersoApp, http://www.persoapp.de
- *
+ * 
  * @version 1.0, 30.07.2013 13:50:47
- *
+ * 
  *          This file is part of PersoApp.
- *
+ * 
  *          PersoApp is free software: you can redistribute it and/or modify it
  *          under the terms of the GNU Lesser General Public License as
  *          published by the Free Software Foundation, either version 3 of the
  *          License, or (at your option) any later version.
- *
+ * 
  *          PersoApp is distributed in the hope that it will be useful, but
  *          WITHOUT ANY WARRANTY; without even the implied warranty of
  *          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  *          Lesser General Public License for more details.
- *
+ * 
  *          You should have received a copy of the GNU Lesser General Public
  *          License along with PersoApp. If not, see
  *          <http://www.gnu.org/licenses/>.
- *
+ * 
  *          Diese Datei ist Teil von PersoApp.
- *
+ * 
  *          PersoApp ist Freie Software: Sie können es unter den Bedingungen der
  *          GNU Lesser General Public License, wie von der Free Software
  *          Foundation, Version 3 der Lizenz oder (nach Ihrer Option) jeder
  *          späteren veröffentlichten Version, weiterverbreiten und/oder
  *          modifizieren.
- *
+ * 
  *          PersoApp wird in der Hoffnung, dass es nützlich sein wird, aber OHNE
  *          JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
  *          Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN
  *          ZWECK. Siehe die GNU Lesser General Public License für weitere
  *          Details.
- *
+ * 
  *          Sie sollten eine Kopie der GNU Lesser General Public License
  *          zusammen mit diesem Programm erhalten haben. Wenn nicht, siehe
  *          <http://www.gnu.org/licenses/>.
- *
+ * 
  */
 
 package de.persoapp.core.ws;
@@ -64,12 +64,7 @@ import de.persoapp.core.card.ICardHandler;
 import de.persoapp.core.client.ECardSession;
 
 /**
- * The <tt>IFDService</tt> is a webservice and allows the transmission of data
- * to connected eCards and the retrieval of the response.
- * 
- * <p>
- * <code>public final class IFDService implements IFD</code>
- * </p>
+ * ISO 24727 IFDService
  * 
  * @author Christian Kahlo
  * @author Rico Klimsa - added javadoc comments.
@@ -78,57 +73,25 @@ import de.persoapp.core.client.ECardSession;
 public final class IFDService implements IFD {
 
 	/**
-	 * The webservice context for access to message contexts and security
-	 * informations relative to a currently served request.
+	 * injected local web service context providing access to message contexts
 	 */
 	@Resource
 	private final WebServiceContext	wsCtx	= null;
 
 	/**
-	 * Initializes the {@link IFDService}.
+	 * container initialization hook
 	 */
 	@PostConstruct
 	public void init() {
 		System.out.println("INIT called: " + wsCtx);
 	}
 
-	/**
-	 * The Transmit function sends one or more APDU(s) to a connected eCard. The
-	 * transmission parameters are divided in two parts.
-	 * <p>
-	 * <table border="1">
-	 * <tr>
-	 * <th>Parameter</th>
-	 * <th>Descryption</th>
-	 * </tr>
-	 * <tr>
-	 * <td>SlotHandle</td>
-	 * <td>With the SlotHandle the connection established with Connect to the
-	 * eCard is addressed.</td>
-	 * </tr>
-	 * <tr>
-	 * <td>InputAPDUInfo</td>
-	 * <td>MAY be present multiple times and contains the command APDU, which is
-	 * sent to the eCard and optionally acceptable status codes.</td>
-	 * </tr>
-	 * </table>
-	 * </p>
+	/*
+	 * (non-Javadoc)
 	 * 
-	 * @param parameters
-	 *            - The <em>parameters</em>, which specifies the data
-	 *            transmission.
-	 * 
-	 * @return The returned Response is divided into two parts
-	 *         <p>
-	 *         <ul>
-	 *         <li>dss:Result - Contains the status information and the errors
-	 *         of an executed action.</li>
-	 *         <li>OutputAPDU - MAY be present multiple times and contains the
-	 *         APDU returned by the eCard. A successful call of the Transmit
-	 *         function MUST contain exactly as many InputAPDU- as
-	 *         OutputAPDU-elements.</li>
-	 *         </ul>
-	 *         </p>
+	 * @see
+	 * iso.std.iso_iec._24727.tech.schema.IFD#transmit(iso.std.iso_iec._24727
+	 * .tech.schema.Transmit)
 	 */
 	@Override
 	public iso.std.iso_iec._24727.tech.schema.TransmitResponse transmit(
@@ -137,13 +100,20 @@ public final class IFDService implements IFD {
 		final TransmitResponse response = new TransmitResponse();
 		response.setRequestID(parameters.getRequestID());
 
+		// retrieve current session from message context
 		final ECardSession session = (ECardSession) wsCtx.getMessageContext().get(ECardSession.class.getName());
+		// retrieve card handler for slot handle
 		final ICardHandler eCardHandler = session.getCardHandler(parameters.getSlotHandle());
 
 		final List<byte[]> outputAPDUList = response.getOutputAPDU();
 		for (final InputAPDUInfoType apdu : parameters.getInputAPDUInfo()) {
 			byte[] apduMsg = apdu.getInputAPDU();
 			if (apduMsg != null && apduMsg.length > 0) {
+				/*
+				 * CLA = 0xFF has a special meaning as it is parsed by the
+				 * terminal driver or the terminal firmware, but not by the
+				 * card. So disallow it for requests from eID server side.
+				 */
 				if (apduMsg[0] != (byte) 0xFF) {
 					apduMsg = eCardHandler.transmit(apduMsg);
 				} else {

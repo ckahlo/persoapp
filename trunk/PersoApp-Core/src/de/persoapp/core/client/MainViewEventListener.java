a7cb880db4a3a0c8722c35780ad0336a1142617a
Christian Kahlo <Christian Kahlo@1296ccec-e324-93b5-48e2-b0d15f74e564>
2014-07-11 08:44:48 +0000

    corrected comments

--- trunk/PersoApp-Core/src/de/persoapp/core/client/MainViewEventListener.java
+++ trunk/PersoApp-Core/src/de/persoapp/core/client/MainViewEventListener.java
@@ -1,49 +1,49 @@
 /**
- *
+ * 
  * COPYRIGHT (C) 2010, 2011, 2012, 2013, 2014 AGETO Innovation GmbH
- *
+ * 
  * Authors Christian Kahlo, Ralf Wondratschek
- *
+ * 
  * All Rights Reserved.
- *
+ * 
  * Contact: PersoApp, http://www.persoapp.de
- *
+ * 
  * @version 1.0, 30.07.2013 13:50:47
- *
+ * 
  *          This file is part of PersoApp.
- *
+ * 
  *          PersoApp is free software: you can redistribute it and/or modify it
  *          under the terms of the GNU Lesser General Public License as
  *          published by the Free Software Foundation, either version 3 of the
  *          License, or (at your option) any later version.
- *
+ * 
  *          PersoApp is distributed in the hope that it will be useful, but
  *          WITHOUT ANY WARRANTY; without even the implied warranty of
  *          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  *          Lesser General Public License for more details.
- *
+ * 
  *          You should have received a copy of the GNU Lesser General Public
  *          License along with PersoApp. If not, see
  *          <http://www.gnu.org/licenses/>.
- *
+ * 
  *          Diese Datei ist Teil von PersoApp.
- *
+ * 
  *          PersoApp ist Freie Software: Sie können es unter den Bedingungen der
  *          GNU Lesser General Public License, wie von der Free Software
  *          Foundation, Version 3 der Lizenz oder (nach Ihrer Option) jeder
  *          späteren veröffentlichten Version, weiterverbreiten und/oder
  *          modifizieren.
- *
+ * 
  *          PersoApp wird in der Hoffnung, dass es nützlich sein wird, aber OHNE
  *          JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
  *          Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN
  *          ZWECK. Siehe die GNU Lesser General Public License für weitere
  *          Details.
- *
+ * 
  *          Sie sollten eine Kopie der GNU Lesser General Public License
  *          zusammen mit diesem Programm erhalten haben. Wenn nicht, siehe
  *          <http://www.gnu.org/licenses/>.
- *
+ * 
  */
 package de.persoapp.core.client;
 
@@ -52,11 +52,8 @@ import de.persoapp.core.card.TransportProvider;
 import de.persoapp.core.client.IMainView.ChangePINDialogResult;
 
 /**
- * The <tt>MainViewEventListener</tt> handles all events related to the running
- * instance of <tt>PersoApp</tt>.
- * <p>
- * <code>public final class MainViewEventListener implements IMainView.EventListener</code>
- * </p>
+ * The <tt>MainViewEventListener</tt> receives events from the GUI and
+ * dispatches them to the core.
  * 
  * @author Christian Kahlo, Ralf Wondratschek
  * @author Rico Klimsa - added javadoc comments.
@@ -69,9 +66,9 @@ public final class MainViewEventListener implements IMainView.EventListener {
 	 * Creates and initializes a new {@link MainViewEventListener}.
 	 * 
 	 * @param eCardHandler
-	 *            - The actual <tt>eCardHandler</tt>.
+	 *            - card handler of the currently running instance
 	 * @param mainView
-	 *            - The actual instance of <tt>PersoApp</tt>
+	 *            - GUI instance of this application
 	 */
 	public MainViewEventListener(final ICardHandler eCardHandler, final IMainView mainView) {
 		this.eCardHandler = eCardHandler;
@@ -79,12 +76,14 @@ public final class MainViewEventListener implements IMainView.EventListener {
 	}
 
 	/**
-	 * Returns the {@link TransportProvider}, which holds the card.
+	 * Searches all terminals for a suitable card and returns a corresponding
+	 * <em>TransportProvider</em>. If a card was found previously and is still
+	 * accessible then the old instance is returned.
 	 * 
 	 * @param requireCard
-	 *            - Set to <strong>true</strong>, if a Card is required.
-	 *            Otherwise <strong>false</strong>.
-	 * @return Returns the Returns the {@link TransportProvider}.
+	 *            - If set to <strong>true</strong> require a card and display a
+	 *            message if no card was found.
+	 * @return {@link TransportProvider} or <em>null</em> if no card was found.
 	 */
 	private final TransportProvider getCard(final boolean requireCard) {
 		TransportProvider tp = null;
@@ -100,6 +99,12 @@ public final class MainViewEventListener implements IMainView.EventListener {
 		return tp;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.persoapp.core.client.IMainView.EventListener#handleEvent(int,
+	 * java.lang.Object[])
+	 */
 	@Override
 	public final Object handleEvent(final int event, final Object[] optionalEventData) {
 		TransportProvider tp = getCard(false);

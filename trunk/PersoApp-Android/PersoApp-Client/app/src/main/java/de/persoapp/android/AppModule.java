43f8ca92b2413c3977f2ad30d4878428490b8cfe
rico.klimsa@ageto.net <rico.klimsa@ageto.net@1296ccec-e324-93b5-48e2-b0d15f74e564>
2014-11-18 17:02:50 +0000

    Added javadoc comments for all public classes. Actualized the file header with the actual year.

--- trunk/PersoApp-Android/PersoApp-Client/app/src/main/java/de/persoapp/android/AppModule.java
+++ trunk/PersoApp-Android/PersoApp-Client/app/src/main/java/de/persoapp/android/AppModule.java
@@ -1,6 +1,6 @@
 /**
  *
- * COPYRIGHT (C) 2010, 2011, 2012, 2013 AGETO Innovation GmbH
+ * COPYRIGHT (C) 2010, 2011, 2012, 2013, 2014 AGETO Innovation GmbH
  *
  * Authors Christian Kahlo, Ralf Wondratschek
  *
